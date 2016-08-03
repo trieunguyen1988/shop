e4930f51d794f5bdc7a3ae13b59aba7b7df3fd7e
Trieu Nguyen <trieu.nguyennhu@gmail.com>
2016-08-03 12:56:01 +0700

    First commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/shop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/shop/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/xampp/htdocs/shop/catalog/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/shop/system/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/shop/image/');
+define('DIR_LANGUAGE', 'D:/xampp/htdocs/shop/catalog/language/');
+define('DIR_TEMPLATE', 'D:/xampp/htdocs/shop/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/xampp/htdocs/shop/system/config/');
+define('DIR_CACHE', 'D:/xampp/htdocs/shop/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/xampp/htdocs/shop/system/storage/download/');
+define('DIR_LOGS', 'D:/xampp/htdocs/shop/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/xampp/htdocs/shop/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/xampp/htdocs/shop/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'sh_');
